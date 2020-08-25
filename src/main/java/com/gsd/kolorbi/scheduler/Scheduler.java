aaac392f75e4e037ef0ca9ea11d6fae968847a2c
Miracle <miracle.nwafor@dochase.com>
2020-08-25 16:49:39 +0100

    Added Jamilakyari News Blog

--- src/main/java/com/gsd/kolorbi/scheduler/Scheduler.java
+++ src/main/java/com/gsd/kolorbi/scheduler/Scheduler.java
@@ -55,37 +55,37 @@ public class Scheduler {
         });
     }
 
-    @Scheduled(cron = "0 */1 * * * *", zone = "Africa/Lagos")
+    @Scheduled(cron = "0 */59 * * * *", zone = "Africa/Lagos")
     public void newsCrawlSchedule() {
 
         for(NewsCrawlerService newsCrawlerService:newsCrawlerServices){
             try {
 
                 System.out.println(newsCrawlerService.getClass().getName());
-//                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.LIBNewsCrawlerServiceImpl"){
-//                    newsCrawlerService.crawlWebsiteForNews();
-//                }
+                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.LIBNewsCrawlerServiceImpl"){
+                    newsCrawlerService.crawlWebsiteForNews();
+                }
 
-//                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.PunchNewsCrawlerServiceImpl"){
-//                    newsCrawlerService.crawlWebsiteForNews();
-//                }
-//                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.TechPointNewsCrawlerServiceImpl"){
-//                    newsCrawlerService.crawlWebsiteForNews();
-//                }
+                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.PunchNewsCrawlerServiceImpl"){
+                    newsCrawlerService.crawlWebsiteForNews();
+                }
+                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.TechPointNewsCrawlerServiceImpl"){
+                    newsCrawlerService.crawlWebsiteForNews();
+                }
 
-//                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.news24NewsCrawlerServiceImpl"){
-//                    newsCrawlerService.crawlWebsiteForNews();
-//                }
+                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.news24NewsCrawlerServiceImpl"){
+                    newsCrawlerService.crawlWebsiteForNews();
+                }
                 if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.JANewsCrawlerServiceImpl"){
                     newsCrawlerService.crawlWebsiteForNews();
                 }
 
-//                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.VanguardNewsCrawlerServiceImpl"){
-//                    newsCrawlerService.crawlWebsiteForNews();
-//                }
-//                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.YouTubeNewsCrawlerServiceImpl"){
-//                    newsCrawlerService.crawlWebsiteForNews();
-//                }
+                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.VanguardNewsCrawlerServiceImpl"){
+                    newsCrawlerService.crawlWebsiteForNews();
+                }
+                if(newsCrawlerService.getClass().getName() == "com.gsd.kolorbi.service.impl.crawler.YouTubeNewsCrawlerServiceImpl"){
+                    newsCrawlerService.crawlWebsiteForNews();
+                }
 
 
             } catch (Exception e) {
